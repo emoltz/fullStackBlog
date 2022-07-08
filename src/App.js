6bfe1f3f41ef83ec0e5aedf773dfd631dbeae511
emoltz <es5541@nyu.edu>
2022-07-08 18:13:54 -0400

    FINALLY updates the stupid React react-router-dom v6 update...

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import './App.css';
-    // bootstrap import: ---->
+// bootstrap import: ---->
 // import './bootstrap-5.2.0-beta1-dist/bootstrap-5.2.0-beta1-dist/css/bootstrap.css'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'jquery/dist/jquery.min.js'
 // import 'bootstrap/dist/js/bootstrap.min.js'
 import Homepage from './pages/HomePage';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Homepage/>
-    </div>
-  );
+import {
+    BrowserRouter,
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <Routes>
+                    <Route path={"/"} element={<Homepage/>}></Route>
+
+                </Routes>
+            </Router>
+        )
+    }
 }
+
 
 export default App;
