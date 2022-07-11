53df26421a471c594dfaaa0cb7bef6e2e85bee15
emoltz <es5541@nyu.edu>
2022-07-11 15:31:45 -0400

    links r workin

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,12 +1,34 @@
 import React from "react";
-import articleContent from './article-content';
+import articles from './article-content';
 
-const ArticlesList = () => (
-    <React.Fragment>
-        <h1>Articles</h1>
+// const ArticlesList = () => (
+//     <React.Fragment>
+//         <h1>Articles</h1>
+//     </React.Fragment>
+//
+// )
 
-    </React.Fragment>
 
-)
+
+function ArticlesList(prop){
+    return(
+        <>
+            <h1 className={"container-fluid"}>Articles</h1>
+            {
+                articles.map(a =>{
+                    return(
+                        <div className={"container"} key={a.key}>
+                            <div className={"container-sm"}>
+                                <a href={`/articles/${a.title}`} className={"link-secondary"}>{a.name}</a>
+
+                            </div>
+
+                        </div>
+                    )
+                })
+            }
+        </>
+    )
+}
 
 export default ArticlesList;
--- src/pages/article-content.js
+++ src/pages/article-content.js
@@ -2,8 +2,27 @@ const articles = [
     {
         name: 'first-entry',
         title: 'This is the first blog post',
+        key: 1,
         content: [
             `Woo! You did it. You really did it. This is online now.`
         ]
+    },
+    {
+        name: 'second-entry',
+        title: 'this is the second entry',
+        key: 2,
+        content: [
+            "Your second article"
+        ]
+    },
+    {
+        name: 'Third-entry',
+        title: 'this is the Third! entry',
+        key: 3,
+        content: [
+            "Your third article. You really did it now."
+        ]
     }
 ]
+
+export default articles;
